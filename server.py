564cecb08549ab8b16461b57a7ff5dad7619a530
inaderi268 <inaderi@310.ai>
2023-04-25 19:03:28 -0600

    init

--- server.py
+++ server.py
@@ -83,5 +83,5 @@ async def create_item(data: ProtNLMDATA):
     names, scores = run_inference(sequence)
     result = {}
     for name, score, i in zip(names, scores, range(len(names))):
-        result[f"Prediction number {i + 1}"]= f"{name} with a score of {score:.03f}"
+        result[f"Prediction number {i + 1}"]= f"**{name}** with a score of {score:.03f}"
     return result
